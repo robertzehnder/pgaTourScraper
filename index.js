0c3ba6610538f9e3d0c5f484b87fb2c892a53a0a
Robert Zehnder <robzcodes@gmail.com>
2016-12-22 18:25:26 -0500

    Adds functioning UI

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var elasticsearch = require('elasticsearch')
+
 var app = express();
+
 var client = elasticsearch.Client({
   host: 'localhost:9200'
 })
@@ -8,6 +10,20 @@ var client = elasticsearch.Client({
 app.listen(4000, () => {
   console.log("app listening on port 4000");
 });
+
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render('index')
+})
+
+app.get("/analytics", (req, res) => {
+  res.render('dashboard')
+})
+
+app.get("/about", (req, res) => {
+  res.render('about')
+})
 
 app.get("/:name", (req, res) => {
   client.search({
